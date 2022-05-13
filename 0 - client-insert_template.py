26a811434ab262c0005e3a38ebf7ca466f0b33a2
jecastrom <jcastro.de@outlook.com>
2022-05-13 23:19:05 +0200

    a

--- 0 - client-insert_template.py
+++ 0 - client-insert_template.py
@@ -7,7 +7,7 @@ URL = "https://2krjwwbp8d.execute-api.us-east-1.amazonaws.com/prod/hello"
 
 
 # read the testfile
-data = pd.read_csv('data/TestSample.csv', sep=',')
+data = pd.read_csv('data1000.csv', sep=',')
 
 # write a single row from the testfile into the api
 #export = data.loc[2].to_json()
