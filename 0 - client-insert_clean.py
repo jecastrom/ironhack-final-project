324dd23e0e4d5b00451239de3738b79d18cc5875
jecastrom <jcastro.de@outlook.com>
2022-05-24 23:23:49 +0200

    a

--- 0 - client-insert_clean.py
+++ 0 - client-insert_clean.py
@@ -11,12 +11,10 @@ df = pd.read_csv('data/data_7000.csv', sep=',')
 # Cleaning data dropping any row with any null value
 df.dropna(axis=0, how='any', inplace=True)
 data = df.copy()
-
 # write a single row from the testfile into the api
 #export = data.loc[2].to_json()
 #response = requests.post(URL, data = export)
 # print(response)
-
 # write all the rows from the testfile to the api as put request
 for i in data.index:
     try:
